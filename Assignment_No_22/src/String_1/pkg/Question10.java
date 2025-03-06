3333d8a61f2ca992ff36a399dcc5d2bad530bd37
Ganesh Bharti <ganeshkundlikbharti728@gmail.com>
2025-03-06 09:59:51 +0530

    solved remaining 2 Questions

--- Assignment_No_22/src/String_1/pkg/Question10.java
+++ Assignment_No_22/src/String_1/pkg/Question10.java
@@ -1,5 +1,45 @@
 package String_1.pkg;
 
+//10.Wap to show an example of method overloading and method overriding. using string.
+
+class Parent{
+	
+	void display(String str)
+	{
+		System.out.println("This parent display method String :"+str);
+	}
+	
+	void display(String str,String str1)
+	{
+		System.out.println("This parent display method String 1st "+str+" 2nd string "+str1);
+	}
+	
+	void print(String str)
+	{
+		System.out.println("This is parent print method string is :"+str);
+	}
+	
+}
+
+class Child extends Parent
+{
+	void print(String str)
+	{
+		System.out.println("This is child class print method :"+str);
+	}
+}
+
+
 public class Question10 {
+
+	public static void main(String[] args) {
+		Parent obj = new Child();
+		Parent ob = new Parent();
+		ob.print("ganesh ");
+		obj.print("ganesh");
+		obj.display("ganesh");
+		obj.display("ganesh", "rushi");
+		
+	}
 
 }
--- Assignment_No_22/src/String_1/pkg/Question11.java
+++ Assignment_No_22/src/String_1/pkg/Question11.java
@@ -1,5 +1,31 @@
 package String_1.pkg;
 
+// 11.Wap enter a string and remove all duplicate character using class and object.
+
 public class Question11 {
+	
+	void removeDuplicate()
+	{
+		String str = "hello java hello program";
+		String str1 ="";
+		for (int i=0;i<str.length();i++)
+		{
+			char ch = str.charAt(i);
+			
+			if (str1.indexOf(ch) == -1)
+			{
+				str1 = str1+ch;
+			}
+			
+		}
+		
+		System.out.println("Remove Duplicate elements String is :"+str1);
+	}
+	
+	public static void main(String[] args) {
+		Question11 obj = new Question11();
+		obj.removeDuplicate();
+		
+	}
 
 }
