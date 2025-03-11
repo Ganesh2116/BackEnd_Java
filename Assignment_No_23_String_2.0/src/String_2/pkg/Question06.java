2eded499afa59366b234e46ec5527da07982ef6f
Ganesh Bharti <ganeshkundlikbharti728@gmail.com>
2025-03-11 22:56:24 +0530

    solved remaining 2Qs

--- Assignment_No_23_String_2.0/src/String_2/pkg/Question06.java
+++ Assignment_No_23_String_2.0/src/String_2/pkg/Question06.java
@@ -1,20 +1,46 @@
 package String_2.pkg;
 
+import java.util.Scanner;
+
 //Q6.Wap enter a string and print first non repeating character using class and object.
 
 
 public class Question06 {
 	
-	String str = "hello p hello ";
-	
-	void firstNoRepeating()
+	static char firstNonrepectingChar(String str)
 	{
-		
+		for (int i=0;i<str.length();i++)
+		{
+			char ch = str.charAt(i);
+			if (str.indexOf(ch) == str.lastIndexOf(ch))
+			{
+				return ch;
+			}
+			
+		}
+		return '\0';
 	}
 	
-	public static void main(String[] args) {
-		Question06 obj = new Question06();
-		obj.firstNoRepeating();
+	public static void main(String[] args) 
+	{
+	   Scanner sc = new Scanner(System.in);
+	   
+	   System.out.println("Enter the String here:");
+	   String str = sc.nextLine();
+	   
+	   char result = firstNonrepectingChar(str);
+	   
+	   if (result != '\0')
+	   {
+		   System.out.println("First Non repeating character is :"+result);
+	   }
+	   else
+	   {
+		   System.out.println("No First Non repeating character is");
+	   }
+	   
+	  
 	}
-
 }
+
+
--- Assignment_No_23_String_2.0/src/String_2/pkg/Question08.java
+++ Assignment_No_23_String_2.0/src/String_2/pkg/Question08.java
@@ -1,19 +1,46 @@
 package String_2.pkg;
 
+import java.util.Scanner;
+
 // Q8.Wap enter a string and print most repeated character in string.
 
 
 public class Question08 {
 	
-	String str ="hello wrold";
-	void mostRepeated()
-	{
-		
-	}
 	
-	public static void main(String[] args) {
-		Question08 obj = new Question08();
-		
-	}
+	 static char getMostRepeatedCharacter(String str) {
+	        if (str.isEmpty()) return '\0'; 
 
+	        int[] freq = new int[256];
+	        char mostRepeated = '\0';
+	        int maxCount = 0;
+
+	        for (int i = 0; i < str.length(); i++) {
+	            char ch = str.charAt(i);
+	            freq[ch]++; 
+
+	            
+	            if (freq[ch] > maxCount) {
+	                maxCount = freq[ch];
+	                mostRepeated = ch;
+	            }
+	        }
+
+	        return mostRepeated;
+	    }
+	
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String str = sc.nextLine();
+	       
+	        char mostRepeatedChar = getMostRepeatedCharacter(str);
+
+	        if (mostRepeatedChar != '\0') {
+	            System.out.println("Most repeated character: " + mostRepeatedChar);
+	        } else {
+	            System.out.println("No repeating character found.");
+	        }
+	    }
 }
